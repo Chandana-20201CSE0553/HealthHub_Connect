d8691b89329b1aa5b9a385b6f8e23b4152da99b3
Nivedita-20201CSE0572 <150114523+Nivedita-20201CSE0572@users.noreply.github.com>
2024-01-15 20:28:09 +0530

    Add files via upload

---
+++ php_include/head.php
@@ -0,0 +1,32 @@
+ <?php include("php_include/connect.php")?>
+ <head>
+               <meta charset="utf-8">
+               <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+               <meta name="author" content="Jthemes"/>
+               <meta name="description" content="MedService - Medical & Medical Health Landing Page Template"/>
+               <meta name="keywords" content="Responsive, HTML5 Template, Jthemes, One Page, Landing, Medical, Health, Healthcare, Doctor, Clinic, Care, Hospital">
+               <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+               <title>Integrated Health Care</title>
+               <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+               <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+               <link rel="apple-touch-icon" sizes="152x152" href="images/apple-touch-icon-152x152.png">
+               <link rel="apple-touch-icon" sizes="120x120" href="images/apple-touch-icon-120x120.png">
+               <link rel="apple-touch-icon" sizes="76x76" href="images/apple-touch-icon-76x76.png">
+               <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+               <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+               <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet">
+               <link href="css/bootstrap.min.css" rel="stylesheet">
+               <link href="../../../../../use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" rel="stylesheet" crossorigin="anonymous">
+               <link href="css/flaticon.css" rel="stylesheet">
+               <link href="css/menu.css" rel="stylesheet">
+               <link id="effect" href="css/dropdown-effects/fade-down.css" media="all" rel="stylesheet">
+               <link href="css/magnific-popup.css" rel="stylesheet">
+               <link href="css/owl.carousel.min.css" rel="stylesheet">
+               <link href="css/owl.theme.default.min.css" rel="stylesheet">
+               <link href="css/animate.css" rel="stylesheet">
+               <link href="css/jquery.datetimepicker.min.css" rel="stylesheet">
+               <link href="css/style.css" rel="stylesheet">
+               <link href="css/responsive.css" rel="stylesheet">
+               <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+            </head>
---
+++ php_include/header.php
@@ -0,0 +1,38 @@
+<header id="header" class="header" >
+                     <div class="wsmobileheader clearfix">
+                        <a id="wsnavtoggle" class="wsanimated-arrow"><span></span></a>
+                        <span class="smllogo"><img src="images/text-1702913677293.png" width="180" height="40" alt="mobile-logo"/></span>
+                        <a href="tel:123456789" class="callusbtn"><i class="fas fa-phone"></i></a>
+                     </div>
+                      <div class="headtoppart bg-blue clearfix" style="height:50px;">
+                        <div class="headerwp clearfix">
+                            
+                          <button type="button" class="btn btn-info" style="padding:15px;"><a href="admin_login.php"><b><font color="white" size="4" >Admin Login</font></b></a></button>
+
+                        </div>
+                     </div>
+                     
+                     <div class="wsmainfull menu clearfix" >
+                        <div class="wsmainwp clearfix" >
+                          <div class="desktoplogo"><a href="#hero-2"><img src="images/text-1702913677293.png"  width="300" height="40" alt="header-logo"></a></div>
+                           <nav class="wsmenu clearfix">
+                              <ul class="wsmenu-list">
+                                 <li aria-haspopup="true">
+                                    <a href="index.php"><font size="4">Home </font></a>
+                                 </li>
+
+                                 <li aria-haspopup="true">
+                                    <a href="doctor_login.php"><font size="4">Doctor Login </font></a>
+                                 </li>
+                                 <li aria-haspopup="true">
+                                    <a href="nurse_login.php"><font size="4">Nurse Login </font></a>
+                                 </li>
+                                 <li aria-haspopup="true">
+                                    <a href="patient_login.php"><font size="4">Patient Login</font> </a>
+                                 </li>
+                                
+                              </ul>
+                           </nav>
+                        </div>
+                     </div>
+                  </header>
