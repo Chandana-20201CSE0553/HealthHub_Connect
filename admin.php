a0acaf9fec8dcb9c9cfe535043b3fc7d70ec2d4a
Chandana-20201CSE0553 <149507615+Chandana-20201CSE0553@users.noreply.github.com>
2024-01-15 19:38:03 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,86 @@
+<html lang="en">
+   <?php include("php_include/head.php")?>
+   <body>
+      <div id="loader-wrapper">
+         <div id="loader">
+            <div class="loader-inner"></div>
+         </div>
+      </div>
+      <div id="page" class="page">
+         <header id="header" class="header" >
+                     
+                     
+                     <div class="wsmainfull menu clearfix" >
+                        <div class="wsmainwp clearfix" >
+                          <div class="desktoplogo"><a href="#hero-2"><img src="images/text-1702913677293.png"  width="300" height="40" alt="header-logo"></a></div>
+                           <nav class="wsmenu clearfix">
+                              <ul class="wsmenu-list">
+                                 <li aria-haspopup="true">
+                                    <a href="admin_register.php"><font size="4">Register & View Members</font></a>
+                                 </li>
+                                 
+                                 
+                                 <li aria-haspopup="true">
+                                    <a href="admin_logout.php"><font size="4">Logout</font></a>
+                                 </li>
+                                 
+                                
+                              </ul>
+                           </nav>
+                        </div>
+                     </div>
+                  </header>
+                  <section id="hero-2" class="hero-section division">
+            <div class="slider blue-nav">
+               <ul class="slides">
+                  <li id="slide-1">
+                     <img src="images/slide1.jpg" alt="slide-background">
+                     <div class="caption d-flex align-items-center left-align">
+                        <div class="container">
+                           <div class="row">
+                              <div class="col-md-9 col-lg-7">
+                                 <div class="caption-txt">
+                                    <h2 class="white-color">Welcome To<br> <span class="white-color">Admin Page</span></h2>
+                                    
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </li>
+               </ul>
+            </div>
+         </section>
+       
+       
+      </div>
+      <script src="js/jquery-3.3.1.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>	
+      <script src="js/modernizr.custom.js"></script>
+      <script src="js/jquery.easing.js"></script>
+      <script src="js/jquery.appear.js"></script>
+      <script src="js/jquery.stellar.min.js"></script>	
+      <script src="js/menu.js"></script>
+      <script src="js/sticky.js"></script>
+      <script src="js/jquery.scrollto.js"></script>
+      <script src="js/materialize.js"></script>	
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/jquery.magnific-popup.min.js"></script>	
+      <script src="js/imagesloaded.pkgd.min.js"></script>
+      <script src="js/isotope.pkgd.min.js"></script>
+      <script src="js/hero-form.js"></script>
+      <script src="js/contact-form.js"></script>
+      <script src="js/comment-form.js"></script>
+      <script src="js/appointment-form.js"></script>
+      <script src="js/jquery.datetimepicker.full.js"></script>		
+      <script src="js/jquery.validate.min.js"></script>	
+      <script src="js/jquery.ajaxchimp.min.js"></script>
+      <script src="js/wow.js"></script>	
+      <script src="js/custom.js"></script>
+      <script> 
+         new WOW().init();
+      </script>
+      <script src="js/changer.js"></script>
+      <script defer src="js/styleswitch.js"></script>	
+   </body>
+</html>
